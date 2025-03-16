dc5731a0b90139b633b65ac3e3a64c1e7862961b
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2025-03-16 15:33:21 +0800

    Revert "feat: :sparkles: Add custom addon"
    This reverts commit 2df68f2b7cd9004eb20d3129361deb1df2dd27d7.

--- widgetbook/lib/main.dart
+++ widgetbook/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
-import 'package:widgetbook_workspace/addon/custom_alignment.dart';
 
 import 'main.directories.g.dart';
 
@@ -51,7 +50,9 @@ class WidgetbookApp extends StatelessWidget {
           ],
         ),
         GridAddon(),
-        CustomAlignmentAddon(),
+        AlignmentAddon(
+          initialAlignment: Alignment.center,
+        ),
       ],
     );
   }
