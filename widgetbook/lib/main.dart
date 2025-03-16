b514a5ac764a090e96c9df52237183701f4e8e42
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2025-03-16 10:17:23 +0800

    feat: :sparkles: Use initial route

--- widgetbook/lib/main.dart
+++ widgetbook/lib/main.dart
@@ -15,6 +15,7 @@ class WidgetbookApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Widgetbook.material(
+      initialRoute: '?path=usertile/primary',
       directories: directories,
     );
   }
