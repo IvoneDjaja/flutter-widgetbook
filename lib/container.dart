98c08c55415101bfc7ebee01456f28b7900ec735
Ivone Djaja <mariaivoneiradjaja@gmail.com>
2025-05-25 14:06:58 +0800

    feat: :sparkles: Add Container color knob

--- lib/container.dart
+++ lib/container.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 class ContainerExample extends StatelessWidget {
-  const ContainerExample({super.key});
+  const ContainerExample({required this.color, super.key});
+
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
+      color: color,
       child: const Text('Container'),
     );
   }
--- widgetbook/lib/container.dart
+++ widgetbook/lib/container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 import 'package:flutter_widgetbook/container.dart';
 
@@ -7,5 +8,11 @@ import 'package:flutter_widgetbook/container.dart';
   type: Container,
 )
 Widget container(BuildContext context) {
-  return const ContainerExample();
+  final color = context.knobs.color(
+    label: 'Color',
+    initialValue: Colors.white,
+  );
+  return ContainerExample(
+    color: color,
+  );
 }
